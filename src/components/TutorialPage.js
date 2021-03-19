c3613a2dc3bce558d691eb8d405948719fd42c98
vigneshiyergithub <vigneshiyer94@gmail.com>
2021-03-19 17:03:02 +0530

    #1 new addition

--- src/components/TutorialPage.js
+++ src/components/TutorialPage.js
@@ -1,16 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import ReactPlayer from 'react-player';
-import {useLocation} from 'react-router-dom';
+// import {useLocation} from 'react-router-dom';
 import { Container, ContentBody, Headline } from '../styledComponents/Unit1';
 import { CodeBlock, dracula } from "react-code-blocks";
 
-const fileMap = {
-    'LandingPage' : 'https://github.com/vigneshiyergithub/joke-guesser-game/blob/main/ui/src/App.js'
-}
-
 const TutorialPage = (props) => {
-    const query = useQuery();
-    const id = query.get('id');
+    // const query = useQuery();
+    // const id = query.get('id');
     const [videoLink, setVideoLink] = useState(null);
     const [codeText, setCodeText] = useState('');
     useEffect(() => {
@@ -41,9 +37,9 @@ const VideoPlayer = (props) => {
     return null;
 }
 
-function useQuery() {
-    return new URLSearchParams(useLocation().search);
-  }
+// function useQuery() {
+//     return new URLSearchParams(useLocation().search);
+//   }
   
 
 export default TutorialPage;
