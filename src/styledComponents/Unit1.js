76101955d49a808bc796452675fdfac933932e0d
vigneshiyergithub <vigneshiyer94@gmail.com>
2021-03-20 12:06:54 +0530

    #1 Adding Tab component

--- src/styledComponents/Unit1.js
+++ src/styledComponents/Unit1.js
@@ -16,6 +16,7 @@ export const ContentBody = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: center;
+    width: 100%;
 `
 
 export const ItemList = styled.ul`
@@ -35,6 +36,8 @@ export const Block = styled.div`
 export const TextBlock= styled.div`
     display: flex;
     flex-direction: row;
+    width: 20vw;
+    padding: 0.6rem;
 `
 
 export const Label = styled.div`
@@ -43,4 +46,25 @@ export const Label = styled.div`
 
 export const Value = styled.div`
 
+`
+
+export const TabContainer = styled.div`
+    display : flex;
+    flex-direction: row;
+    width: 50vw;
+    margin-top: 10rem;
+    justify-content: space-between;
+`
+// Center 
+export const TabLabel = styled.div`
+    text-align: center;
+    border: 2px solid black;
+    width: 6vw;
+    cursor: pointer;
+    background-color: ${props => props.selected ? 'gray' : 'none'}
+`
+export const TabContentContainer = styled.div`
+    border: 0.5px solid gray;
+    width: 50vw;
+    margin-top: 5rem;
 `
--- src/tutorials/PropsSpreadOperator.js
+++ src/tutorials/PropsSpreadOperator.js
@@ -1,68 +1,11 @@
 import React from 'react';
-import { Container, ContentBody } from '../styledComponents/Unit1'
-import PersonDataDisplay from './sub-components/PersonDataDisplay'
+import Tabs from './Tabs';
+import {TAB_CONFIG} from './sub-components/tabConfig'
 
 const PropsSpreadOperator = (props) => {
-    // TODO: maybe add a Tab component and navigate different examples
-    return <Example2 />
-}
-
-/*
-    Scenario : Usually when there is a large number of props to be passed which can be easily consolidated in a object
-*/
-const Example1 = (props) => {
-    const componetProps = {
-        name : 'Person Name',
-        age: 20
-    }
-    return <Container marginTop={'5rem'}>
-        <ContentBody>
-            <PersonDataDisplay {...componetProps}/> 
-        </ContentBody>
-    </Container>
-}
-
-/*
-    Scenario : When component props is sourced from multiple sources.
-    Variations: 
-        - {...firstProps} {...secondProps}
-        - {...{...firstProps, ...secondProps}}
-*/
-const Example2 = (props) => {
-    const firstProps = {
-        name : 'Person Name'
-    }
-    const secondProps = {
-        age : 20
-    }
-    return <Container marginTop={'5rem'}>
-    <ContentBody>
-        <PersonDataDisplay {...firstProps} {...secondProps} /> 
-    </ContentBody>
-</Container>
-}
-
-/*
-    Scenario : Override props and adding props
-    Caution: overridding props : order is important
-    Variations:
-        {...componetProps} name={'Vignesh'}
-        name={'Vignesh} {...componetProps}
-        {...partialProps} name={'Vignesh}
-*/
-const Example3 = (props) => {
-    const componetProps = {
-        name : 'Person Name',
-        age: 20
-    }
-    // const partialProps = {
-    //     name : 'Person Name'
-    // }
-    return <Container marginTop={'5rem'}>
-        <ContentBody>
-            <PersonDataDisplay {...componetProps} name={'Vignesh'} /> 
-        </ContentBody>
-    </Container>
+    return <Tabs
+        config={TAB_CONFIG}
+    />
 }
 
 export default PropsSpreadOperator;
