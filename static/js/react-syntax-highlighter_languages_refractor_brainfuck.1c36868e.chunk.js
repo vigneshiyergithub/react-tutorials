d354403da0b0d5e97f1cb1c26016821363528fa2
vigneshiyergithub <vigneshiyer94@gmail.com>
2021-03-20 12:25:43 +0530

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_brainfuck.1c36868e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-tutorials"]=this["webpackJsonpreact-tutorials"]||[]).push([[18],{155:function(t,a,e){"use strict";function n(t){t.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}t.exports=n,n.displayName="brainfuck",n.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.1c36868e.chunk.js.map
---
+++ static/js/react-syntax-highlighter_languages_refractor_hsts.cce8f86e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-tutorials"]=this["webpackJsonpreact-tutorials"]||[]).push([[55],{188:function(a,s,t){"use strict";function e(a){a.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\d{8,}/,alias:"selector"},unsafe:{pattern:/\d{1,7}/,alias:"function"}}}a.exports=e,e.displayName="hsts",e.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts.cce8f86e.chunk.js.map
