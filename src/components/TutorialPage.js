f2485445c8e0fd8bf327cc10dd7c6f19ac12dcf8
vigneshiyergithub <vigneshiyer94@gmail.com>
2021-03-25 20:45:04 +0530

    Adding resources block

--- src/components/TutorialPage.js
+++ src/components/TutorialPage.js
@@ -23,16 +23,24 @@ import ReactEmbedGist from 'react-embed-gist';
 //     transform: scale(0.5);
 //     height: 3rem;
 // `
-//TODO: Add Resources block to display all the relevant references
+
+const DEFAULT_TUTORIAL_DETAILS = {
+    link: null,
+    title: '',
+    videoLink: '',
+    gist: '',
+    resources: []
+}
+
 const TutorialPage = (props) => {
-    const [tutorialDetails, setTutorialDetails] = useState({});
+    const [tutorialDetails, setTutorialDetails] = useState(DEFAULT_TUTORIAL_DETAILS);
     const query = useQuery();
     const id = query.get('id');
     useEffect(() => {
         const tutorialDetails = TutorialMapping[id];
         setTutorialDetails(tutorialDetails);
     }, [id]);
-    const { title, link, gist, videoLink } = tutorialDetails;
+    const { title, link, gist, videoLink, resources } = tutorialDetails;
     return <Card width={'80vw'}>
         <Container>
             <Navbar />
@@ -46,12 +54,33 @@ const TutorialPage = (props) => {
             <Headline>Related Files</Headline>
             <WidthComponent width={'80%'}>
                 <ReactEmbedGist gist={gist} />
+                <Resources resources={resources} />
             </WidthComponent>
             <EmptyBlock />
             <GitLink />
             <EmptyBlock />
         </Container>
     </Card>
+}
+
+const Resources = (props) => {
+    const { resources } = props;
+    if (resources.length > 0) {
+        return <>
+        <Headline>Resources</Headline>
+        <ul>
+            {
+                resources.map((resource, index) => <li key={index}>
+                    <a href={resource}
+                        target={'_blank'}
+                        rel='noreferrer'
+                    >{resource}</a>
+                </li>)
+            }
+        </ul>
+        </>
+    }
+    return null;
 }
 
 const VideoPlayer = (props) => {
--- src/constants/Tutorial-Mapping.js
+++ src/constants/Tutorial-Mapping.js
@@ -1,19 +1,26 @@
 export const Tutorials = {
-    PropsSpreadOperator : 'props-spread-operator',
+    PropsSpreadOperator: 'props-spread-operator',
     ClickChildRef: 'click-child-ref'
 }
 
 export const TutorialMapping = {
-    [`${Tutorials.PropsSpreadOperator}`] : {
-        link : Tutorials.PropsSpreadOperator,
+    [`${Tutorials.PropsSpreadOperator}`]: {
+        link: Tutorials.PropsSpreadOperator,
         title: 'How to pass props to child component using spread operator',
-        videoLink : '',
-        gist: 'vigneshiyergithub/fe8e30a543dfc7db5a0132ed909f3e4c'
+        videoLink: '',
+        gist: 'vigneshiyergithub/fe8e30a543dfc7db5a0132ed909f3e4c',
+        resources: [
+            'https://reactjs.org/docs/jsx-in-depth.html#spread-attributes',
+            'https://kentcdodds.com/blog/prop-drilling',
+            'https://reactjs.org/docs/context.html',
+            'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax'    
+        ]
     },
-    [`${Tutorials.ClickChildRef}`] : {
+    [`${Tutorials.ClickChildRef}`]: {
         title: 'How to trigger child click event using refs',
-        videoLink : '',
-        gist: 'vigneshiyergithub/6c41293706893bb057af7977292adbc5'
+        videoLink: '',
+        gist: 'vigneshiyergithub/6c41293706893bb057af7977292adbc5',
+        resources: []
     }
 };
 
